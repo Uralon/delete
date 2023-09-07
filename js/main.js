5ec62afd72d767e6cee7d48ff43a28f6ea390778
Uralon <misterlwolz272@yandex.ru>
2023-09-07 15:41:24 +0300

    d

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-function alertClick(){
-    alert("Привет");
-}
+let nameС = prompt('Название Города');
+let yearC = prompt('Год образования');
+let year = 2023 - yearC;
+let number = prompt('Население');
 
-alert("Привет");
+
+alert("Городу " + nameС + " исполнилось " + year + " лет с момента его образования. " + number + " - количество человек");
